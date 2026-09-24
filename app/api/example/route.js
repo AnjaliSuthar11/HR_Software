f1c8d942085a7c54f1dd43af50cf4421418291f2
Your Name <anjalisuthar114@gmail.com>
2026-09-24 12:02:10 +0530

    24th september

--- app/api/example/route.js
+++ app/api/example/route.js
@@ -1,44 +1,10 @@
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
-  try {
-    const { email, password } = await request.json();
 
-    if(!email){
-        return NextResponse.json({message:"Email is not field"})
-    }
-    if (!email || !password) {
-      return NextResponse.json(
-        {
-          message: "Email and password are required",
-        },
-        {
-          status: 400,
-        }
-      );
-    }
 
-    if (email === "anjali@gmail.com" && password == "123456") {
-      return NextResponse.json(
-        {
-          message: "login successfully",
-        },
-        {
-          status: 200,
-        }
-      );
-    }
 
-    return NextResponse.json({ message: "401 unauthorized " }, { status: 401 });
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message: "something went wrong",
-      },
-      { status: 500 }
-    );
-  }
-}
+// original code 
+// 21st september added login for this dashboard
 
 export async function POST(request) {
   try {
@@ -61,6 +27,7 @@ export async function POST(request) {
 
     return NextResponse.json({ message: "401 Unauthorized" }, { status: 401 });
   } 
+  
   catch (error) {
     return NextResponse.json(
       { message: "Something went wrong" },
